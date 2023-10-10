090ed8637ffe5d27078e30d7b681d53297a2aace
QuentinESIEE <quentin.guyot@edu.esiee.fr>
2023-10-10 15:11:21 +0200

    1st commit

---
+++ client/src/main/java/clientcom/esiee/careandpark/client/modele/Vehicule.java
@@ -0,0 +1,23 @@
+package clientcom.esiee.careandpark.client.modele;
+
+import clientcom.esiee.careandpark.client.modele.reference.TypeVehicule;
+
+public class Vehicule {
+	
+	private TypeVehicule type;
+	
+	public Vehicule(TypeVehicule type) {
+		this.type = type;
+	}
+
+	public TypeVehicule getType() {
+		return type;
+	}
+
+	public void setType(TypeVehicule type) {
+		this.type = type;
+	}
+	
+	
+
+}
